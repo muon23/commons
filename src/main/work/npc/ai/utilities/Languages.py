b93613515b7994db63dbeb0684bb3df8edd0d4c6
CJ Wang <cj.wang@npc.work>
2021-06-16 13:25:43 -0700

    Changed Languages.sentences() to preserve original text and mark positions of the starts of the sentences.

--- src/main/work/npc/ai/utilities/Languages.py
+++ src/main/work/npc/ai/utilities/Languages.py
@@ -2,6 +2,7 @@ import re
 import string
 
 import nltk
+from typing import Tuple, Generator
 from zhon import zhuyin, hanzi
 
 nltk.download('punkt')
@@ -9,7 +10,7 @@ nltk.download('punkt')
 
 class Languages:
     _quoteStart = r"\[({'\"«‹“‘（［｛｟｢〈《「『【"
-    _quoteEnd = r"\])}'\"»›”’）］｝｠｣〉》」』】"
+    _quoteEnd = r"])}'\"»›”’）］｝｠｣〉》」』】"
 
     _punctuation = re.compile(f"[{_quoteEnd}{_quoteStart}{hanzi.punctuation}{string.punctuation}\n ]")
     _ideography = re.compile(f"[{hanzi.characters}{hanzi.radicals}{zhuyin.characters}]")
@@ -48,26 +49,26 @@ class Languages:
 
         return ideographic, alphabetical
 
-    englishStops = ".?!"
-    englishEndQuotes = "[\"'”“]"
-    hanziStops = hanzi.stops + englishStops
-    hanziSentence = re.compile(f"[^{hanziStops}]+[{hanziStops}]")
-    englishSentence = re.compile(f"([^{englishStops}]+[{englishStops}]+{englishEndQuotes}?)(\\s+|$)")
-    spaces = re.compile(r"\s+")
-    leadingBlanks = re.compile(r"^[\t\n ]+")
-    trailingBlanks = re.compile(r"[\t\n ]+$")
-
+    _englishStops = ".?!"
+    _englishEndQuotes = "\"'”“"
+    _stops = hanzi.stops + _englishStops
+    _hanziSentence = re.compile(f"[^{_stops}]+[{_stops}]")
+    _englishSentence = re.compile(f"([^{_englishStops}]+[{_englishStops}]+[{_englishEndQuotes}]?)(\\s+|$)")
+    _spaces = re.compile(r"\s+")
+    _leadingBlanks = re.compile(r"^[\t\n ]+")
+    _trailingBlanks = re.compile(r"[\t\n ]+$")
+    _blanks = " \n\t\r"
 
     @staticmethod
     def _fixText(text):
         # Fixing spaces
         text.strip()
-        text = Languages.spaces.sub(" ", text)
-        text = Languages.leadingBlanks.sub("", text)
-        text = Languages.trailingBlanks.sub("", text)
+        text = Languages._spaces.sub(" ", text)
+        text = Languages._leadingBlanks.sub("", text)
+        text = Languages._trailingBlanks.sub("", text)
 
         # If not ended with a period, add one
-        if text[-1:] not in Languages.hanziStops + Languages.englishEndQuotes:
+        if text[-1:] not in Languages._stops + Languages._englishEndQuotes:
             text += "."
 
         return text
@@ -76,7 +77,6 @@ class Languages:
         "No", "Mr", "Mrs", "Ms", "Messrs", "Mmes", "Dr", "Prof", "Rev", "Sen", "Hon", "St", "MD", "PhD",
         "Mt", "Jr", "Sr",
     ]
-
     _dot = "%%PERIOD%%"
 
     @staticmethod
@@ -86,7 +86,7 @@ class Languages:
             result = result.replace(ab + ". ", f"{ab}{Languages._dot} ")
 
         result = re.sub(
-            f"[^{Languages.englishStops}]([{Languages.englishStops}])((?!(\\s|[\n]|$))|(?={Languages.englishEndQuotes}))",
+            f"[^{Languages._englishStops}]([{Languages._englishStops}])((?!(\\s|[\n]|$))|(?={Languages._englishEndQuotes}))",
             lambda matched: matched.group(0).replace(".", Languages._dot),
             result
         )
@@ -101,7 +101,7 @@ class Languages:
     def hanziSentences(text):
         text = Languages._fixText(text)
         text = Languages._replaceDots(text)
-        result = Languages.hanziSentence.findall(text)
+        result = Languages._hanziSentence.findall(text)
         return [Languages._recoverDots(s) for s in result]
 
     @staticmethod
@@ -110,8 +110,80 @@ class Languages:
         text = Languages._replaceDots(text)
         # return nltk.tokenize.sent_tokenize(text)
         # My rule is better than NLTK's
-        result = [s[0] for s in Languages.englishSentence.findall(text)]
+        result = [s[0] for s in Languages._englishSentence.findall(text)]
         return [Languages._recoverDots(s) for s in result]
+
+    @staticmethod
+    def sentences(text: str) -> Generator[Tuple[int, str], None, None]:
+        sentence = ""
+        sentenceBegin = 0
+        i = 0
+
+        while i < len(text):
+
+            if sentence == "":
+                # If we are starting to look for a new sentence, skipping leading blanks
+                while i < len(text) and text[i] in Languages._blanks:
+                    i += 1
+
+                if i == len(text):   # End of text
+                    break
+
+                sentenceBegin = i
+
+            sentence += text[i]
+
+            if text[i] in Languages._stops:
+                # We see a . ! ? or other potential sentence stops.  Let's see if it is really a sentence
+
+                if text[i+1] in Languages._quoteEnd:
+                    # An end quote after sentence stop.  Include all the end quotes afterward into the sentence.
+                    k = i+2
+                    while k < len(text) and text[k] in Languages._quoteEnd:
+                        k += 1
+                    sentence += text[i+1:k]
+                    yield sentenceBegin, sentence
+                    sentence = ""
+
+                    if k == len(text): # End of text
+                        break
+
+                    i = k-1
+
+                elif text[i+1] in Languages._blanks:
+                    # A blank after stop, potentially a sentence unless it is a abbreviation of titles (Mr., Dr.) or a
+                    # numbered item (1. 2. 3.).
+
+                    # Some of the exceptions below could actually be the end of a sentence, but those conditions
+                    # happens rarer than when those occurring in the middle of a sentence.
+
+                    # Go backward to find the last space
+                    k = i - 1
+                    while k > sentenceBegin and text[k] not in Languages._blanks:
+                        k -= 1
+                    lastWord = text[k+1:i]
+
+                    if lastWord in Languages._abbreviations:  # Mr./Dr./Mt., not the end of a sentence
+                        pass
+
+                    elif re.match(r"\d+", lastWord):  # An item of a numbered list
+                        pass
+
+                    elif re.match(r"[.]+", lastWord):  # It is like ...
+                        pass
+
+                    elif re.match(r"(\w+[.])+\w+", lastWord):  # Abbreviation like A.B.C., Ph.D.
+                        pass
+
+                    else:
+                        yield sentenceBegin, sentence
+                        sentence = ""
+
+            i += 1
+
+        if sentence != "":
+            # End of the text reached and there are still things left over in the sentence
+            yield sentenceBegin, sentence
 
     @staticmethod
     def implySentences(text):
