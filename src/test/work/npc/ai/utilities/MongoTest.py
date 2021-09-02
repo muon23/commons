d7446dad0dd287b1ecbd210a0d70509104ac2d9b
muon23 <83474230+muon23@users.noreply.github.com>
2021-09-01 21:05:09 -0700

    Updated README and changed unit test to deployment/local.

--- src/test/work/npc/ai/utilities/MongoTest.py
+++ src/test/work/npc/ai/utilities/MongoTest.py
@@ -6,16 +6,26 @@ from work.npc.ai.utilities.Mongo import Mongo
 
 
 class MongoTest(unittest.TestCase):
-    mongoServer = os.environ.get("MONGO_UAT")
+    mongoUat = os.environ.get("MONGO_UAT")
+    mongoProd = os.environ.get("MONGO_PROD")
 
     def test_access(self):
-        server = f"{MongoTest.mongoServer}?authSource=habitat&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+        server = f"{self.mongoUat}?authSource=habitat&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
         mongo = Mongo(server, "habitat")
         query = {"localId": "723eb555-b642-41a9-9ba1-69b43a041e49"}
         docs = list(mongo.get("note", query))
         print(docs)
 
         self.assertGreater(len(docs), 0)
+
+    def test_accessImageCaptions(self):
+        server = f"{self.mongoProd}?authSource=habitat&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+        mongo = Mongo(server, "habitat")
+        query = {}
+        docs = list(mongo.get("ImageVideoSearch", query))
+        for doc in docs:
+            print("====")
+            print(f"{doc['OCR']} -- {doc['ImageCaption']}")
 
 
 if __name__ == '__main__':
