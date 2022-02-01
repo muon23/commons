5ea308a71ddf302c11d0b8247aebdb51638e210b
muon23 <83474230+muon23@users.noreply.github.com>
2022-02-01 12:18:30 -0800

    Updated README.  Printed MongoDB query to log.

--- src/main/work/npc/ai/utilities/Mongo.py
+++ src/main/work/npc/ai/utilities/Mongo.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from typing import TypeVar
 
@@ -14,8 +15,10 @@ class Mongo:
                 raise ValueError(f"Missing database info in {uri}")
             database = m[1] if len(m.groups()) >= 1 else None
 
+        self.uri = uri
         self.client = pymongo.MongoClient(uri)
         self.db = self.client[database]
 
     def get(self, collection: str, query: dict):
+        logging.info(f"Querying MongoDB {self.uri} with {query}")
         return self.db[collection].find(query)
